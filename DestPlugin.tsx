63076c03051622c636edc97c8d1d8456736bc83d
segment-voliveira <115718482+segment-voliveira@users.noreply.github.com>
2024-04-02 12:20:01 +0100

    Create DestPlugin.tsx

---
+++ DestPlugin.tsx
@@ -0,0 +1,28 @@
+import {
+    Plugin,
+    PluginType,
+    SegmentEvent
+  } from '@segment/analytics-react-native';
+  import { Platform, NativeModule } from 'react-native';
+
+  import UserAgent from 'react-native-user-agent';
+  
+  export class DestPlugin extends Plugin {
+    // Note that `type` is set as a class property
+    // If you do not set a type your plugin will be a `utility` plugin (see Plugin Types above)
+    type = PluginType.enrichment;
+  
+    execute(event: SegmentEvent) {
+      console.log(UserAgent.getUserAgent());
+      if (Platform.OS !== 'android') {
+        return;
+      }
+      
+      if (event.context !== undefined) {
+        event.context['userAgent'] = UserAgent.getUserAgent();
+      }
+      
+      return event;
+    }
+  
+  }
