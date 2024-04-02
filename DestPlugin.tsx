c1c2e09db98672e397929f42d75739b086c9deb4
segment-voliveira <115718482+segment-voliveira@users.noreply.github.com>
2024-04-02 12:20:31 +0100

    Update DestPlugin.tsx

--- DestPlugin.tsx
+++ DestPlugin.tsx
@@ -6,7 +6,9 @@ import {
   import { Platform, NativeModule } from 'react-native';
 
   import UserAgent from 'react-native-user-agent';
-  
+
+//Adds user agent info to events before sent downstream
+
   export class DestPlugin extends Plugin {
     // Note that `type` is set as a class property
     // If you do not set a type your plugin will be a `utility` plugin (see Plugin Types above)
